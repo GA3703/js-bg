faa1a219a7733e4b28467900cf8704720215829e
GA3703 <157503394+GA3703@users.noreply.github.com>
2024-10-29 19:56:47 +0000

    dt

---
+++ .devcontainer/01_learn/1.1init.js
@@ -0,0 +1,9 @@
+const accountId = 3256253
+let accountEmail = "gagan@gamial.com"
+var accountPassword = "32562476"
+let accountState;
+accountCity = "jalandhar"
+
+console.table([accountId, accountEmail,accountPassword,accountState,accountCity]);
+
+ 
---
+++ .devcontainer/01_learn/1.1initdt.js
@@ -0,0 +1,20 @@
+//data types
+
+let name ="gagan"
+let age = 21
+let isLoggedIn = true
+
+//number range = 2^53
+//boolean = true/false
+//string = ""
+//bigint= large numbers
+//null = standalone
+// undefined = yet to be declared
+//symbol = for uniqueness
+
+//object
+
+console.log(typeof undefined);//undefined
+
+console.log(typeof null);//object
+
