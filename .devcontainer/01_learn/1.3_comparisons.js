f99a9a51e108de0d2592fd07866ba205d0d3ff7f
GA3703 <157503394+GA3703@users.noreply.github.com>
2024-11-02 16:17:30 +0000

    1.3_comparisons

---
+++ .devcontainer/01_learn/1.3_comparisons.js
@@ -0,0 +1,26 @@
+console.log(2>1)
+console.log(1<2)
+console.log(2==2)
+console.log(2>=1)
+console.log(2!=1)
+
+//strict check (===) along with the value it also check the datatype
+
+console.log(null>0)
+console.log(null<0)
+console.log(null>=0)
+
+console.log("2">1)
+console.log(2>"1")
+
+//increment/decrement
+
+let a = 3
+const b = a++ //postfix
+console.log(a , b)
+// a = 4 , b = 3 
+
+let j = 6
+const k = ++j // prefix
+console.log(j ,k )
+// a = 7 , k = 7
